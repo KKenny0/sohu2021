f64bf40877d01dce87b93f96dbb3728f480e1544
Kenny <jdlow@live.cn>
2022-03-27 22:33:32 +0800

    modify run.sh

--- code/run.sh
+++ code/run.sh
@@ -3,6 +3,6 @@ python train.py --do_train --do_eval --bert_model models/RoBERTa --saving_path o
 #python train.py --do_train --do_eval --bert_model models/WoBERT --saving_path output/WoBERT --learning_rate 4e-5 --num_epoch 10 --batch_size 32 --fp16 --do_ema --do_adv
 #python train.py --do_train --do_eval --bert_model models/WWM --saving_path output/WWM --learning_rate 4e-5 --num_epoch 10 --batch_size 32 --fp16 --do_ema --do_adv
 #test
-python train.py --do_infer --saving_path output/RoBERTa --bert_model models/RoBERTa
-#python train.py --do_eval --saving_path output/WoBERT --bert_model models/WoBERT
-#python train.py --do_eval --saving_path output/WWM --bert_model models/WWM
+python train.py --do_eval --do_infer --saving_path output/RoBERTa --bert_model models/RoBERTa
+#python train.py --do_eval --do_infer --saving_path output/WoBERT --bert_model models/WoBERT
+#python train.py --do_eval --do_infer --saving_path output/WWM --bert_model models/WWM
